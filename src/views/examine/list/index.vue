b4dd75ee241ebbefb2dfe265110022ba5936a4a4
fengmuyan <37791395+fengmuyan@users.noreply.github.com>
2020-04-10 20:44:41 +0800

    last

--- src/views/examine/list/index.vue
+++ src/views/examine/list/index.vue
@@ -3,6 +3,20 @@
     <el-collapse-transition>
       <div class="form-p" v-if="formShow" ref="formPublic" v-resize="resize">
         <el-form :model="queryForm" ref="queryForm" :inline="true">
+          <el-form-item label="用户类型" prop="usertype">
+            <el-select
+              v-model="queryForm.usertype"
+              placeholder="请选择"
+              clearable
+              size="small"
+              style="width: 240px"
+            >
+              <el-option label="供应商" value="1" />
+              <el-option label="经销商" value="2" />
+              <el-option label="个人" value="0" />
+              <el-option label="平台用户" value="3" />
+            </el-select>
+          </el-form-item>
           <el-form-item label="认证状态" prop="state">
             <el-select
               v-model="queryForm.state"
@@ -51,15 +65,27 @@
         </el-col>
       </el-row>
       <el-table style="width: 100%" v-loading="loading" :data="userList">
-        <el-table-column label="用户编号" prop="usercode" />
+        <el-table-column label="用户编号" prop="usercode" width="150" />
         <el-table-column label="用户账号" prop="useraccount" />
-        <el-table-column label="注册类型" prop="voRegistertype" />
-        <el-table-column label="用户类型" prop="voUsertype" width="100px"/>
+        <el-table-column label="注册类型" sortable prop="voRegistertype" />
+        <el-table-column label="用户类型" sortable prop="voUsertype" width="100px" />
         <el-table-column label="手机号" prop="phone" />
-        <el-table-column label="创建时间" prop="voCreatetime" width="150px"/>
-        <el-table-column label="审核状态" prop="state" width="100px">
-          <template slot-scope="scope"><span :class="{'warn-color':scope.row.state===2,'suc-color':scope.row.state===3}">{{scope.row.state | initState }}</span></template>
+        <el-table-column label="店铺名称" prop="shopname" width="120" />
+        <el-table-column label="收款账户" sortable prop="merstate" width="120">
+          <template slot-scope="scope">
+            <span
+              :class="{'warn-color':scope.row.merstate===2,'suc-color':scope.row.merstate===1}"
+            >{{scope.row.merstate | initAccountState }}</span>
+          </template>
         </el-table-column>
+        <el-table-column label="审核状态" sortable prop="state" width="100px">
+          <template slot-scope="scope">
+            <span
+              :class="{'warn-color':scope.row.state===2,'suc-color':scope.row.state===3}"
+            >{{scope.row.state | initState }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="创建时间" sortable prop="voCreatetime" width="150px" />
         <el-table-column label="操作" width="100px">
           <template slot-scope="scope">
             <el-button size="mini" type="text" icon="el-icon-edit" @click="handleEdit(scope.row)">查看</el-button>
@@ -78,7 +104,7 @@
 </template>
 <script>
 import { getExamineList } from "@/api/examine";
-import minHeightMix from '@/mixins/minHeight'
+import minHeightMix from "@/mixins/minHeight";
 export default {
   mixins: [minHeightMix],
   data() {
@@ -91,7 +117,8 @@ export default {
       queryForm: {
         pageNum: 1,
         pageSize: 10,
-        state: undefined
+        state: undefined,
+        usertype: undefined
       }
     };
   },
@@ -99,6 +126,15 @@ export default {
     initState(val) {
       const arr = ["待认证", "认证中", "认证失败", "认证成功"];
       return arr[val];
+    },
+    initAccountState(val) {
+      if (val === null || val === undefined || val === "") {
+        return "-";
+      } else {
+        return ["审核中", "审核通过", "审核未通过", "待验证", "解约", "已关闭"][
+          val
+        ];
+      }
     }
   },
   created() {
@@ -138,17 +174,17 @@ export default {
       const { useraccount, registertype } = item;
       let path;
       if (Number(registertype) === 0) {
-        path = `/examine/personal-detail/${useraccount}`
+        path = `/examine/personal-detail/${useraccount}`;
       } else if (Number(registertype) === 1 || Number(registertype) === 2) {
-        path = `/examine/business-detail/${useraccount}`
+        path = `/examine/business-detail/${useraccount}`;
       }
       this.$router.push({ path });
     },
     _initParams(obj) {
       const dateRange = this.dateRange || [];
       Object.assign(obj, {
-        beginTime: dateRange.length > 0 ? dateRange[0] : null,
-        endTime: dateRange.length > 0 ? dateRange[1] : null
+        beginTime: dateRange.length > 0 ? dateRange[0] : undefined,
+        endTime: dateRange.length > 0 ? dateRange[1] : undefined
       });
       return obj;
     }
